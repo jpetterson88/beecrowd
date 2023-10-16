bb65ea05717dc11c6f4ed42d2f381d7c5729c67c
John Peterson <116783976+jpetterson88@users.noreply.github.com>
2023-10-16 17:40:07 -0300

    Update 2520

--- 2520.js
+++ 2520.js
@@ -3,7 +3,11 @@ var lines = input.split("\r\n");
 
 let matriz = [];
 let i, j;
-let dois, um;
+let pos1Linha;
+let pos1Coluna;
+let pos2Linha;
+let pos2Coluna;
+let diferenca;
 
 while(lines.length > 1) {
     let [linhas, colunas] = lines.shift().split(" ").map(Number);
@@ -15,8 +19,15 @@ while(lines.length > 1) {
     for(i = 0; i < linhas; i++) {
         for(j = 0; j < colunas; j++) {
             if(matriz[i][j] == '2') {
-                dois = 
+                pos2Linha = i;
+                pos2Coluna = j;
+            }
+            if(matriz[i][j] == '1') {
+                pos1Linha = i;
+                pos1Coluna = j;
             }
         }
     }
+    diferenca = Math.abs(pos1Linha - pos2Linha) + Math.abs(pos1Coluna - pos2Coluna);
+    console.log(diferenca);
 }
