21dc18acf6e1c557db8e082e839da3daecbe0b2e
John Peterson <116783976+jpetterson88@users.noreply.github.com>
2023-10-17 02:04:17 -0300

    updade 2523

--- 2523.js
+++ 2523.js
@@ -4,7 +4,20 @@ var lines = input.split("\r\n");
 let letras;
 let totLampadas;
 let lamPiscadas;
+let i, j, texto;
 
 while(lines.length > 1) {
     letras = lines.shift().split("");
+    totLampadas = lines.shift();
+    lamPiscadas = lines.shift().split(" ");
+    texto = "";
+
+    for(i = 0; i < totLampadas; i++) {
+        for(j = 0; j < letras.length; j++) {
+            if(lamPiscadas[i] == j + 1) {
+                texto += letras[j];
+            }
+        }
+    }
+    console.log(texto);
 }
