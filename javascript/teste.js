d5a51c5548dbf1a07436ac57b4b7c8e0747a36e3
John Peterson <116783976+jpetterson88@users.noreply.github.com>
2023-10-31 00:32:39 -0300

    Update teste.js

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,6 +1,7 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\r\n');
 
-let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+x = 10;
+y = 20;
 
-console.log(matrix.length);
+console.log(x + y);
